db5799c4985488fd6bc327bebfd959b95d9381c5
richomielorenzo <derpyspiderman@gmail.com>
2023-02-06 19:42:01 -0800

    removed colab

--- detection_custom.py
+++ detection_custom.py
@@ -16,11 +16,11 @@ import tensorflow as tf
 from yolov3.utils import detect_image, detect_realtime, detect_video, Load_Yolo_model, detect_video_realtime_mp
 from yolov3.configs import *
 
-image_path   = "D:/Documents/TensorFlow for Social Good/tf_1/IMAGES/lateblight.jpg"
+image_path   = "D:/Documents/TensorFlow-for-Social-Good/IMAGES/tomato.jpg"
 #video_path   = ""
 
 yolo = Load_Yolo_model()
-detect_image(yolo, image_path, "D:/Documents/TensorFlow for Social Good/tf_1/IMAGES/lateblight_d.jpg", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+detect_image(yolo, image_path, "D:/Documents/TensorFlow-for-Social-Good/IMAGES/tomato_d.jpg", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
 #detect_video(yolo, video_path, "", input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
 #detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255, 0, 0))
 
