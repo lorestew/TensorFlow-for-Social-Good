c670aaab7b6a8edacebb8b11a635cb5c9a5e005e
richomielorenzo <derpyspiderman@gmail.com>
2023-02-06 19:31:15 -0800

    Update detection_custom.py

--- detection_custom.py
+++ detection_custom.py
@@ -16,12 +16,12 @@ import tensorflow as tf
 from yolov3.utils import detect_image, detect_realtime, detect_video, Load_Yolo_model, detect_video_realtime_mp
 from yolov3.configs import *
 
-image_path   = "./IMAGES/plate_2.jpg"
-video_path   = "./IMAGES/test.mp4"
+image_path   = "D:/Documents/TensorFlow for Social Good/tf_1/IMAGES/lateblight.jpg"
+#video_path   = ""
 
 yolo = Load_Yolo_model()
-detect_image(yolo, image_path, "./IMAGES/plate_1_detect.jpg", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
-#detect_video(yolo, video_path, './IMAGES/detected.mp4', input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+detect_image(yolo, image_path, "D:/Documents/TensorFlow for Social Good/tf_1/IMAGES/lateblight_d.jpg", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+#detect_video(yolo, video_path, "", input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
 #detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255, 0, 0))
 
 #detect_video_realtime_mp(video_path, "Output.mp4", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0), realtime=False)
